d74cc2c9b80db0a3004b0c79a1eb56227c7a763f
zhangchunsheng <zhangchunsheng@bytedance.com>
2024-07-27 23:59:00 +0800

    Initial Commit

---
+++ NutriVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriVision
+//
+//  Created by 张春生 on 2024/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
